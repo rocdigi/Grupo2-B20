198182020e09bfeeb51e08f8f7d49e83797596c2
Johan Roa <jmdata11@gmail.com>
2021-11-29 22:09:13 -0500

    package.json update

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ class Server {
     
     constructor(){
         this.app = express();
-        this.port = process.env.PORT || 5000;
+        this.port = process.env.PORT || 3000;
         this.personsPath = '/api/persons';
         this.questionsPath = '/api/questions';
         //this.answersPath = '/api/answers';
